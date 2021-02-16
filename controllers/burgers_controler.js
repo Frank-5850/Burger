62660ceab3c8f14118278098c22550fd7b825f43
Franco <phanfranco@yahoo.com>
2021-02-15 22:43:57 -0800

    Finished burger controllers.

---
+++ controllers/burgers_controler.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../Models/burger");
+
+router.get("/", (req, res) => {
+  res.send("This is a test");
+  burger.selectAll((data) => {
+    const handleBarsObject = { burgers: data };
+    console.log(handleBarsObject);
+    res.render(handleBarsObject);
+  });
+});
+
+router.post("/burger/create", (req, res) => {
+  burger.insertOne(req.body.burger_name, () => {
+    console.log(res);
+  });
+});
+
+router.post("/burger/eat", (req, res) => {
+  burger.updateOne(req.body.id, () => {
+    console.log(res);
+  });
+});
+
+router.post("/burger/uneat", (req, res) => {
+  burger.updateOne(req.body.id, () => {
+    console.log(res);
+  });
+});
+
+module.exports = router;
