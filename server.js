d353361e5d15d9854cf9eac31524ef45304a7e74
Franco <phanfranco@yahoo.com>
2021-02-19 19:14:30 -0800

    Created basic handlebars file structure and updated server to output simple data.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
+const exphbs = require("express-handlebars");
+const burger = require("./controllers/burgers_controller");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("/", burger);
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
